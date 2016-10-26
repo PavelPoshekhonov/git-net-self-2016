1dbbacb9cbaec613484b6d4caaa691b57ea90615
Pavel Poshekhonov <pipmail@mail.ru>
2016-10-26 17:28:58 +0300

    CalculateBonus
    Добавление перечислений ReturnCodes, Departments и Ranks

--- C#/Intro/CalculateBonus/Program.cs
+++ C#/Intro/CalculateBonus/Program.cs
@@ -8,6 +8,31 @@ namespace CalculateBonus
 {
     class Program
     {
+        // Возвращаемые значения
+        enum ReturnCodes : int {
+            AllOK = 0,      // Премия успешно начислена
+            NoArg = -1,     // Не был передан обязательный (0й) аргумент
+            WrongDep = -2,  // Указаный департамент не существует
+            WrongRank = -3  // Указанная должность не существует
+        }
+
+        // Отделы
+        enum Departments : int {
+            RD = 0,         // Отдел разработок
+            QA,             // Отдел тестирования
+            Support,        // Отдел технической поддержки
+            Sales,          // Отдел продаж
+            Marketing       // Отдел маркетинга
+        }
+
+        // Должности
+        enum Ranks : int {
+            Employee = 0,   // Сотрудник
+            Lead,           // Ведущий сотрудник
+            Manager,        // Руководитель
+            Director        // Директор
+        }
+
         static void Main(string[] args)
         {
         }
