ebc576ca5ff660d6753b665fea2c387257121bec
Pavel Poshekhonov <pipmail@mail.ru>
2016-11-11 16:58:05 +0300

    C# 01 TankWars
    Создание классов

---
+++ C#/C# 01/TankWars/GameObjects.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace TankWars
+{
+    // Направление движения
+    public enum Direction { Left, Right, Top, Bottom }
+
+    // Базовый класс для игровых объектов: Яблоко, Колобок, Танк, Пуля
+    public class GameObject
+    {
+        Rectangle position;
+        public Rectangle Position       // Текущее положение объекта
+        {
+            get { return position; }
+            set
+            {
+                OldPosition = Position;
+                position = value;
+                LastMoving = DateTime.Now;
+                // Инициировать перерисовку
+            }
+        }
+    }
+
+    // Базовый класс для игровых объектов: Колобок, Танк, Пуля
+    public class MovingObject : GameObject
+    {
+        Rectangle position;
+        public Rectangle Position       // Текущее положение объекта
+        {
+            get { return position; }
+            set
+            {
+                OldPosition = Position;
+                position = value;
+                LastMoving = DateTime.Now;
+                // Инициировать перерисовку
+            }
+        }
+        public Rectangle OldPosition;       // Предыдущее положение объекта
+        public DateTime LastMoving;         // Время последнего перемещения
+        public int MovementDelay;           // Определяет как часто происходит перемещение объекта, мсек
+        public Direction direction;
+
+        public MovingObject()
+        {
+            // Выбрать свое место на игровом поле
+            // Задать position
+        }
+    }
+
+    // Колобок
+    public class Kolobok : MovingObject
+    {
+    }
+
+    // Танк
+    public class Tank : MovingObject
+    {
+    }
+
+}
+
---
+++ C#/C# 01/TankWars/ViewObjects.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using TankWars.GameObjects;
+
+namespace TankWars
+{
+    // Отобразить игровой объект
+    public class GameObjectView
+    {
+        public GameObjectView()
+        {
+        }
+    }
+
+    public class KolobokView
+    {
+    }
+
+    public class TankView
+    {
+    }
+
+
+}
