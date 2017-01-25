4f9966e5d7cab472e69406293bdefa41a40d77d6
Pavel Poshekhonov <pipmail@mail.ru>
2017-01-25 17:24:34 +0300

    C# 01 FormatCoords
    Работа над форматированным выводом

--- C#/C# 01/FormatCoords/Program.cs
+++ C#/C# 01/FormatCoords/Program.cs
@@ -8,19 +8,32 @@ using System.IO;
 
 namespace FormatCoords
 {
-    ///<summary>Представляет пару координат x и y в двухмерном пространстве (на плоскости).</summary>
+    /// <summary>Представляет пару координат x и y в двухмерном пространстве (на плоскости).</summary>
     public struct Point
     {
         public decimal X { get; set; }
         public decimal Y { get; set; }
     }
 
-    ///<summary>Предназначен для хранения и форматированного вывода массива координат <see cref="Point"/>.</summary>
+    /// <summary>Предназначен для хранения и форматированного вывода списка координат <see cref="Point"/>.</summary>
     public class CoordSet
     {
+        public string Sourse { get; }
         List<Point> coords = new List<Point>();
 
-        ///<summary>Добавляет координату в массив координат.</summary>
+        int maxXpre = 0;
+        int maxXpost = 0;
+        int maxYpre = 0;
+
+        /// <summary>Конструктор.</summary>
+        /// <param name="sourse">Имя источника списка координат (Консоль / Файл).</param>
+        public CoordSet(string sourse)
+        {
+            Sourse = sourse;
+        }
+
+        /// <summary>Добавляет координату в список координат.</summary>
+        /// <param name="coordStr">Строка, содержащая пару координат x и y.</param>
         public void Add(string coordStr)
         {
             string[] strXY;
@@ -44,27 +57,33 @@ namespace FormatCoords
             newPoint.X = x;
             newPoint.Y = y;
 
+            maxXpre  = Math.Max(maxXpre,  strXY[0].IndexOf(DecSep));
+            maxXpost = Math.Max(maxXpost, strXY[0].Length - strXY[0].IndexOf(DecSep) - 1);
+            maxYpre  = Math.Max(maxXpre,  strXY[1].IndexOf(DecSep));
+
             coords.Add(newPoint);
         }
 
-        ///<summary>Производит форматированный вывод массива координат.</summary>
+        /// <summary>Производит форматированный вывод списка координат.</summary>
         public void MakeOutput()
         {
             foreach (Point crd in coords)
             {
-                Console.WriteLine("X = " + crd.X.ToString() + " Y = " + crd.Y.ToString());
+                Console.WriteLine(string.Format("X: {0:#######.#######} Y: {1:#######.#######}", crd.X, crd.Y));
             }
         }
     }
 
 
+    /// <summary>Консольное приложение для чтения, форматирования и отображения чисел, представляющих x,y координаты положения объекста.</summary>
     class Program
     {
-        ///<summary>Обеспечивает ввод координат пользователем с консоли.</summary>
+        /// <summary>Обеспечивает ввод координат пользователем с консоли.</summary>
+        /// <returns>Экземпляр <see cref="CoordSet"/>.</returns>
         static CoordSet MakeConsoleInput()
         {
             string strLine;
-            CoordSet cs = new CoordSet();
+            CoordSet cs = new CoordSet("Ввод с консоли");
 
             do
             {
@@ -79,7 +98,8 @@ namespace FormatCoords
             return cs;
         }
 
-        ///<summary>Обеспечивает ввод координат из текстовых файлов.</summary>
+        /// <summary>Обеспечивает ввод координат из текстовых файлов.</summary>
+        /// <returns>Массив списков координат <see cref="CoordSet"/>.</returns>
         static List<CoordSet> MakeFileInput(string[] args)
         {
             List<CoordSet> csl = new List<CoordSet>();
@@ -87,7 +107,7 @@ namespace FormatCoords
             for (int i = 0; i < args.Length; i++)
             {
                 StreamReader sr = new StreamReader(args[i]);
-                csl.Add(new CoordSet());
+                csl.Add(new CoordSet("Данные из файла " + args[i]));
 
                 while (sr.EndOfStream == false)
                 {
@@ -102,31 +122,31 @@ namespace FormatCoords
         static void Main(string[] args)
         {
             // Массив списков координат. (Каждый файл загружается в свой список.)
-            List<CoordSet> coordList = new List<CoordSet>();
+            List<CoordSet> coordSet = new List<CoordSet>();
 
             // Выбор режима работы
             if (args.Length == 0)
             {
                 // Ввод с консоли
                 Console.WriteLine("FormatCoords. Запуск без аргументов. Чтение с консоли.");
-                coordList.Add(MakeConsoleInput());
+                coordSet.Add(MakeConsoleInput());
             }
             else
             {
                 // Загрузка из текстовых файлов
                 Console.WriteLine("FormatCoords. Запуск с аргументами. Чтение из файлов.");
-                coordList = MakeFileInput(args);
+                coordSet = MakeFileInput(args);
             }
 
             // Форматированный вывод
-            foreach (CoordSet cf in coordList)
+            foreach (CoordSet cs in coordSet)
             {
-                Console.WriteLine("FormatCoords. Форматированный вывод.");
-                cf.MakeOutput();
+                Console.WriteLine(cs.Sourse);
+                cs.MakeOutput();
             }
 
             Console.WriteLine("");
-            Console.WriteLine("FormatCoords. Нажмите Enter.");
+            Console.WriteLine("Нажмите Enter.");
             Console.ReadLine();
         }
     }
